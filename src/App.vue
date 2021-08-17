af2eeece364ae126665bd4f12d9494610dbe5b20
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-08-17 17:55:11 +0530

    vue3-getting-started for Badge

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div id='element'>
+    <h1>Badge Component <span class="e-badge e-badge-primary">New</span></h1>
+</div>
+</template>
+
+<script>
+import "../node_modules/@syncfusion/ej2-vue-notifications/styles/material.css";
+
+export default {
+    name: "App"
+}
+</script>
+<style>
+
+#element {
+    display: flex;
+    width: 400px;
+    margin: auto;
+    border: 1px solid #dddddd;
+    border-radius: 3px;
+    justify-content: center;
+}
+</style>
